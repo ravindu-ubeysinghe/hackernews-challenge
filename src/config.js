032d73c591a92c5aa6a241da2dd4c42a51115a56
ravindu.ubeysinghe <ravindu.ubeysinghe@beteasy.com.au>
2020-05-01 22:38:52 +1000

    feat: added news id fetcher function

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_BASE: 'https://hacker-news.firebaseio.com/v0',
+};
+
+export default config;
--- src/views/News/News.tsx
+++ src/views/News/News.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import config from 'config';
 
 const Home: React.FC = () => {
-    return <div>Content goes here</div>;
+    const [popNewsIds, setPopNewsIds] = useState<null | object>(null);
+    const fetchPopNewsIds = (): void => {
+        axios.get(`${config.API_BASE}/topstories.json`).then((res) => {
+            setPopNewsIds(res);
+        });
+    };
+
+    useEffect(() => {
+        fetchPopNewsIds();
+    }, []);
+
+    return <div>{JSON.stringify(popNewsIds)}</div>;
 };
 
 export default Home;
